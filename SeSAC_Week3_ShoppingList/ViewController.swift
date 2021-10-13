37b6b881e40b3c546fa4793a2306d8a1c6a7da2b
SehunKang <andriy.kang@gmail.com>
2021-10-13 21:27:06 +0900

    Initial Commit

---
+++ SeSAC_Week3_ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Week3_ShoppingList
+//
+//  Created by Sehun Kang on 2021/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
