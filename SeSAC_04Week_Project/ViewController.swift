59a450be60b0a22140b78d956493d747b230c2e7
SehunKang <andriy.kang@gmail.com>
2021-10-18 09:29:27 +0900

    Initial Commit

---
+++ SeSAC_04Week_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_04Week_Project
+//
+//  Created by Sehun Kang on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
