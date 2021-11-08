e01006d46c9e3859ebbee5dadde5e5ca84c6b5ce
SehunKang <andriy.kang@gmail.com>
2021-11-08 19:42:46 +0900

    Initial Commit

---
+++ SeSAC_MyMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_MyMemoApp
+//
+//  Created by Sehun Kang on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
