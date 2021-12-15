b7438ce89759dd136a08abb7cbc8eb51aa30add0
SehunKang <andriy.kang@gmail.com>
2021-12-15 19:22:13 +0900

    Initial Commit

---
+++ Carrot_Prac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carrot_Prac
+//
+//  Created by Sehun Kang on 2021/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
