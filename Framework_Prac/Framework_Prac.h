760a10722959719b06370b137b19691c244d728f
SehunKang <andriy.kang@gmail.com>
2021-12-23 09:33:23 +0900

    Initial Commit

---
+++ Framework_Prac/Framework_Prac.h
@@ -0,0 +1,18 @@
+//
+//  Framework_Prac.h
+//  Framework_Prac
+//
+//  Created by Sehun Kang on 2021/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework_Prac.
+FOUNDATION_EXPORT double Framework_PracVersionNumber;
+
+//! Project version string for Framework_Prac.
+FOUNDATION_EXPORT const unsigned char Framework_PracVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_Prac/PublicHeader.h>
+
+
